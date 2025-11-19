5bf51a7b5959aaeade18608c816781d7e21a6f3c
duplosentido <thangtranthe.77@gmail.com>
2025-11-19 12:59:04 +0700

    Change Navbar import path to use double quotes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from "./Navbar";
+
 import ProductDetail from './components/ProductDetail';
 import BeautyAI from './components/BeautyAI';
 
